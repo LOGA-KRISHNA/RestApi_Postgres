87685134221a3d297d30c45a0f8c43f881ae12ba
loga-krishna <logakrishnad@gmail.com>
2024-07-09 21:52:26 +0530

    Second Coomit

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ import env from 'dotenv';
 import product from './routes/product.js';
 import order from './routes/order.js';
 import bodyParser from 'body-parser';
-
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 env.config();
